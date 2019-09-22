fc6eb42c6ce26b6d6bcdcb0a132f342913e23f36
David Wyss <davidwyss@Davids-Mac-Pro.local>
2019-09-22 17:35:56 +0200

    Add initial tested version

--- sport-to-mavlink/sport-to-mavlink.ino
+++ sport-to-mavlink/sport-to-mavlink.ino
@@ -10,10 +10,7 @@
 #include "FrSkySportSensor.h"
 #include "FrSkySportSensorAss.h"
 #include "FrSkySportSensorFcs.h"
-#include "FrSkySportSensorFlvss.h"
 #include "FrSkySportSensorGps.h"
-#include "FrSkySportSensorRpm.h"
-#include "FrSkySportSensorSp2uart.h"
 #include "FrSkySportSensorVario.h"
 #include "FrSkySportSingleWireSerial.h"
 #include "FrSkySportDecoder.h"
@@ -25,10 +22,6 @@
 FrSkySportSensorAss ass;                               // Create ASS sensor with default ID
 FrSkySportSensorFcs fcs;                               // Create FCS-40A sensor with default ID (use ID8 for FCS-150A)
 FrSkySportSensorGps gps;                               // Create GPS sensor with default ID
-FrSkySportSensorRpm rpm;                               // Create RPM sensor with default ID
-FrSkySportSensorFlvss flvss1;                          // Create FLVSS sensor with default ID
-FrSkySportSensorFlvss flvss2(FrSkySportSensor::ID15);  // Create FLVSS sensor with given ID
-FrSkySportSensorSp2uart sp2uart;                       // Create SP2UART Type B sensor with default ID
 FrSkySportSensorVario vario;                           // Create Variometer sensor with default ID
 #ifdef POLLING_ENABLED
   FrSkySportDecoder decoder(true);                     // Create decoder object with polling
@@ -58,19 +51,21 @@ uint32_t upTime = 0;          // System uptime, usually set to 0 for cases where
 float roll = 0;         // Roll angle in degrees
 float pitch = 0;        // Pitch angle in degrees
 float yaw = 0;          // Yaw angle in degrees
-int16_t heading = 0;      // Geographical heading angle in degrees
-float lat = 47.379945;    // GPS latitude in degrees (example: 47.123456)
-float lon = 8.539970;     // GPS longitude in degrees
+int16_t heading = 0;    // Geographical heading angle in degrees
+float lat = 0.0;        // GPS latitude in degrees (example: 47.123456)
+float lon = 0.0;        // GPS longitude in degrees
 float alt = 0.0;        // Relative flight altitude in m
+float lat_temp = 0.0;   //Temp values for GPS coords
+float lon_temp = 0.0; 
 float groundspeed = 0.0; // Groundspeed in m/s
 float airspeed = 0.0;    // Airspeed in m/s
-float climbrate = 0.0;    // Climb rate in m/s, currently not working
-float throttle = 0.0;     // Throttle percentage
+float climbrate = 0.0;   // Climb rate in m/s, currently not working
+float throttle = 0.0;    // Throttle percentage
 
 // GPS parameters
-int16_t gps_sats = 0;    // Number of visible GPS satellites
-int32_t gps_alt = 0.0;  // GPS altitude (Altitude above MSL)
-float gps_hdop = 100.0;     // GPS HDOP
+int16_t gps_sats = 0;     // Number of visible GPS satellites
+int32_t gps_alt = 0.0;    // GPS altitude (Altitude above MSL)
+float gps_hdop = 100.0;   // GPS HDOP
 uint8_t fixType = 0;      // GPS fix type. 0-1: no fix, 2: 2D fix, 3: 3D fix
 
 // Battery parameters
@@ -78,91 +73,92 @@ float battery_remaining = 0.0;  // Remaining battery percentage
 float voltage_battery = 0.0;    // Battery voltage in V
 float current_battery = 0.0;    // Battery current in A
 
-
-
+// Sensor decoder setup, defaults to Pin 2 (D2 on Arduino Nano)
 void setup() {
     // Configure the decoder serial port and sensors (remember to use & to specify a pointer to sensor)
   #if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MKL26Z64__) || defined(__MK66FX1M0__) || defined(__MK64FX512__)
-    decoder.begin(FrSkySportSingleWireSerial::SERIAL_3, &ass, &fcs, &flvss1, &flvss2, &gps, &rpm, &sp2uart, &vario);
+    decoder.begin(FrSkySportSingleWireSerial::SERIAL_2, &ass, &fcs, &gps, &vario);
   #else
-    decoder.begin(FrSkySportSingleWireSerial::SOFT_SERIAL_PIN_12, &ass, &fcs, &flvss1, &flvss2, &gps, &rpm, &sp2uart, &vario);
+    decoder.begin(FrSkySportSingleWireSerial::SOFT_SERIAL_PIN_2, &ass, &fcs, &gps, &vario);
   #endif
 
-  //Serial defaults to 57600 bauds, can be set higher if desired
-  Serial.begin(57600);
+  //Serial defaults to 115200 bauds
+  Serial.begin(115200);
 }
-
+ 
 
 // Main loop: decode S.Port telemetry and pass onwards in MAVLink format
 void loop() {
   
-  // Decode S.port input data
+  // Decode S.port input data and send
   decodeSportData();
-  
-  // Send MAVLink heartbeat
-  command_heartbeat(system_id, component_id, system_type, autopilot_type, system_mode, custom_mode, system_state);
 
-  //Send battery status
-  command_status(system_id, component_id, battery_remaining, voltage_battery, current_battery);
-
-  // Send GPS and altitude data
-  command_gps(system_id, component_id, upTime, fixType, lat, lon, alt, gps_alt, heading, groundspeed, gps_hdop, gps_sats);
-
-  // Send HUD data (speed, heading, climbrate etc.)
-  command_hud(system_id, component_id, airspeed, groundspeed, heading, throttle, alt, climbrate);
-
-  // Send attitude data to artificial horizon
-  command_attitude(system_id, component_id, upTime, roll, pitch, yaw);
-
-  //TODO: delay?
-  //delay(100)
-
+  //Send data every second (1hz) during decoding
 }
 
 
 
 /************************************************************
-* @brief Decodes the inbound S.Port stream and sets the telemetry variables correspondingly
+* @brief Decodes the inbound S.Port stream and sets the telemetry variables correspondingly, then passes MAVLink data forward
 * @param NONE
 * @return void
 *************************************************************/
 
 void decodeSportData(){
   // Read and decode the telemetry data, note that the data will only be decoded for sensors
-  // that that have been passed to the begin method. Print the AppID of the decoded data.
+  // that that have been passed to the begin method
   decodeResult = decoder.decode();
-  if(decodeResult != SENSOR_NO_DATA_ID) { Serial.print("Decoded data with AppID 0x"); Serial.println(decodeResult, HEX); }
   
-  // Display data once a second to not interfeere with data decoding TODO check need for this
+  // Display data once a second to not interfeere with data decoding
   currentTime = millis();
   if(currentTime > displayTime)
   {
     displayTime = currentTime + 1000;
 
     // Get airspeed sensor (ASS) data
-    airspeed = ass.getSpeed(); // Airspeed in km/h
+    //airspeed = ass.getSpeed(); // Airspeed in km/h
   
     // Get current/voltage sensor (FCS) data
     current_battery = fcs.getCurrent(); // Current consumption in amps
     voltage_battery = fcs.getVoltage(); // Battery voltage in volts
-  
+    
     // Get GPS data
-    lat = gps.getLat();                 // GPS Latitude
-    lon = gps.getLon();                 // GPS Longitude in degrees decimal (positive for N/E, negative for S/W)
+    lat_temp = gps.getLat();                 // GPS Latitude
+    lon_temp = gps.getLon();                 // GPS Longitude in degrees decimal (positive for N/E, negative for S/W)
     gps_alt = gps.getAltitude();       // Altitude in m (can be negative)
     groundspeed = gps.getSpeed();      // Speed in m/s
-    fixType = 3;                        // Manually set GPS to 3D fix if position is obtained
-    heading = gps.getCog();            // Course over ground in degrees (0-359, 0 = north)
-    yaw = heading;  //Set yaw to heading value
 
-    //TODO check whether we need this data
-    // Get SP2UART sensor data (ADC3 and ADC4 voltage in V)
-    //Serial.print("SP2UART: ADC3 = "); Serial.print(sp2uart.getAdc3());                // ADC3 voltage in volts
-    //Serial.print("V, ADC4 = "); Serial.print(sp2uart.getAdc4()); Serial.println("V"); // ADC4 voltage in volts
-  
+    if(lat_temp != 0.0 && lon_temp != 0.0){
+      lat = lat_temp;
+      lon = lon_temp;
+      fixType = 3; // Manually set GPS to 3D fix if position is obtained
+    }           
+    else{
+      fixType = 1; // Manually set No GPS fix
+    }            
+    //heading = gps.getCog();            // Course over ground in degrees (0-359, 0 = north)
+    //yaw = heading;  //Set yaw to heading value
+    
     // Get variometer data (altitude in m, VSI in m/s)
     alt = vario.getAltitude();       // Altitude in meters (can be negative)
-    climbrate = vario.getVsi();       // Vertical speed in m/s (positive - up, negative - down)
+    //climbrate = vario.getVsi();       // Vertical speed in m/s (positive - up, negative - down)
+
+
+    // Send MAVLink heartbeat
+    command_heartbeat(system_id, component_id, system_type, autopilot_type, system_mode, custom_mode, system_state);
+  
+    //Send battery status
+    command_status(system_id, component_id, battery_remaining, voltage_battery, current_battery);
+  
+    // Send GPS and altitude data
+    command_gps(system_id, component_id, upTime, fixType, lat, lon, alt, gps_alt, heading, groundspeed, gps_hdop, gps_sats);
+  
+    // Send HUD data (speed, heading, climbrate etc.)
+    //command_hud(system_id, component_id, airspeed, groundspeed, heading, throttle, alt, climbrate);
+  
+    // Send attitude data to artificial horizon
+    //command_attitude(system_id, component_id, upTime, roll, pitch, yaw);
+  
 
   }  
 }
